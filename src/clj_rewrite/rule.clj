e22d9b81a612de11194497ffb4ae9f0458619495
Alien Science <saul@alienscience.org.uk>
2011-02-11 11:47:55 +0100

    Separated rewrite functions from rewrite language

--- src/clj_rewrite/rule.clj
+++ src/clj_rewrite/rule.clj
@@ -1,7 +1,6 @@
 
 (ns clj-rewrite.rule
-  "Rule building for clj-rewrite"
-  (:use clojure.walk))
+  "Rule building for clj-rewrite")
 
 (def binds* nil)
 
@@ -149,58 +148,6 @@
                        :else       [x])) 
                    contents))))
 
-(defn- split-symbol
-  "Splits a symbol into name and last character strings.
-   Returns [name last-char] -sif last character is + or *
-   or returns [name nil]."
-  [s]
-  (let [as-str (str s)
-        end (.length as-str)
-        last-char (.substring as-str (dec end) end)]
-    (if (contains? #{"+" "*"} last-char)
-      [(.substring as-str 0 (dec end)) last-char]
-      [as-str nil])))
-
-(defn- convert-match-symbol
-  "Converts a symbol specifying a match into a functional
-   form"
-  [s]
-  (let [[name last-char] (split-symbol s)
-        k (keyword name)]
-    (set! binds* (conj binds* k))
-    (condp = last-char
-      "+"   `(match+ ~k)
-      "*"   `(match* ~k)
-      `(match ~k))))
-
-
-(declare convert-match-seq)
-
-(defn- convert-match-element
-  "Converts an element used for matching into a functional form"
-  [s]
-  (cond
-    (symbol? s)                      (convert-match-symbol s)
-    (and (seq? s) 
-         (not= 'quote (first s)))    (convert-match-seq s)
-    :else                            s))
-
-(defn- convert-match-seq
-  "Converts a sequence used for matching into a functional form"
-  [s]
-  (walk convert-match-element vec s))
-
-(defn- convert-match
-  "Converts a datastructure used to specify matches into
-   a functional form. Returns the new datastructure and
-   a vector of keys that will be set by matching." 
-  [ds]
-  (binding [binds* []]
-    (let [pattern (apply vector
-                         (list 'quote (first ds))
-                         (convert-match-seq (next ds)))]
-      [pattern binds*])))
-
 (defn rule
   "Return a function that implements the given substitution rule."
   ([pattern subs-fn] (rule pattern nil subs-fn))
@@ -209,95 +156,3 @@
        (fn [d]
          (if-let [m (match-fn d)]
            (subs-fn m))))))
-
-(defn- destructure-when
-  "Destructures a map from a match to provide bindings for a
-   when statement"
-  [binds]
-  (let [symbols (map #(-> % name symbol) binds)]
-    `{:keys [~@symbols]}))
-
-(defn- build-when-fn
-  "Converts a when specification into a function definition"
-  [spec binds]
-  (let [destructuring (destructure-when binds)]
-    `(fn [m#]
-       (let [~destructuring m#]
-         ~spec))))
-
-(declare convert-substitution)
-
-(defn- convert-sub-element
-  "Converts an element of a substitution into functional form"
-  [e]
-  (cond
-    (symbol? e)    `(sub ~(keyword e))
-    (seq? e)       (convert-substitution e)
-    :else          e))
-
-(defn- get-builder
-  "Returns symbol identifying the function to build the 
-   given substitution specification"
-  [sub-spec first-element]
-  (cond
-    ;;---
-    (list? sub-spec)    
-    ['build-list 
-     (if (symbol? first-element) 
-       (list 'quote first-element) 
-       (convert-sub-element first-element))]
-    ;;---
-    :else
-    (throw
-     (.Exception 
-      (str "Don't know how to build a subtitution from "
-           (type sub-spec))))))
-
-(defn- convert-substitution
-  "Converts a substitution specification into a function
-   definition"
-  [sub-spec]
-  (let [builder (get-builder sub-spec (first sub-spec))
-        s (map convert-sub-element (next sub-spec))]
-    `(~@builder ~@s)))
-
-
-(defn parse-rewrite
-  "Returns a sequence of rewrite rules in a functional form
-   from the spec of a def-rewrite"
-  [raw]
-  (if-not (empty? raw)
-    (let [[match-spec when when-spec sub-spec] (take 4 raw)]
-      (cond
-        ;---
-        (and (= :when when))
-        (let [[pattern binds] (convert-match match-spec)
-              when-fn (build-when-fn when-spec binds)
-              subs-fn (convert-substitution sub-spec)]
-          (cons `(rule ~pattern ~when-fn ~subs-fn)
-                (lazy-seq (parse-rewrite (nthnext raw 4)))))
-        ;---
-        (not (nil? when))
-        (let [[pattern] (convert-match match-spec)
-              subs-fn (convert-substitution when)]
-          (cons `(rule ~pattern ~subs-fn)
-                (lazy-seq (parse-rewrite (nthnext raw 2)))))
-        ;---
-        :else
-        (throw 
-         (.Exception 
-          (str "Malformed rule beginning with " match-spec)))))))
-
-;; TODO: support optional documentation string
-(defmacro def-rewrite
-  "Defines a set of rewrite rules"
-  [identifier & body]
-  (let [h (first body)
-        doc-string (if (string? h) h)]
-    (if doc-string
-      (let [func-form (parse-rewrite (next body))]
-        `(def ~(with-meta identifier {:doc doc-string})
-              [~@func-form]))
-      (let [func-form (parse-rewrite body)]
-        `(def ~identifier
-              [~@func-form])))))
